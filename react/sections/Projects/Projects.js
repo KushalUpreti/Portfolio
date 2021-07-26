ff5ccae9a9ecb32fe200b847e3968b859b0641df
Kushal Upreti <ehunt9030@gmail.com>
2021-07-26 14:14:22 +0545

    Completed projects section

--- react/sections/Projects/Projects.js
+++ react/sections/Projects/Projects.js
@@ -7,10 +7,46 @@ export default function Projects() {
             My Projects
         </h2>
         <ul>
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+            <Project
+                image_src="/assets/react-chat.png"
+                title="React-Chat"
+                project_link="https://github.com/KushalUpreti/React-Chat"
+                project_desc={"Qui aliquip voluptate nisi id non ad reprehenderit aliqua. Reprehenderit duis in labore labore nisi nostrud" +
+                    "estsint reprehenderit elit dolore proident. Tempor qui id do eu irure nulla nostrud eiusmod."}
+                tech_stack="Next.js &nbsp; React &nbsp; SocketIO &nbsp; Express"
+                hosted_link="https://kushalupreti.github.io/ReactChat"
+            />
+
+            <Project
+                image_src="/assets/react-chat.png"
+                title="React-Chat"
+                project_link="https://github.com/KushalUpreti/React-Chat"
+                project_desc={"Qui aliquip voluptate nisi id non ad reprehenderit aliqua. Reprehenderit duis in labore labore nisi nostrud" +
+                    "estsint reprehenderit elit dolore proident. Tempor qui id do eu irure nulla nostrud eiusmod."}
+                tech_stack="Next.js &nbsp; React &nbsp; SocketIO &nbsp; Express"
+                hosted_link="https://kushalupreti.github.io/ReactChat"
+            />
+
+            <Project
+                image_src="/assets/react-chat.png"
+                title="React-Chat"
+                project_link="https://github.com/KushalUpreti/React-Chat"
+                project_desc={"Qui aliquip voluptate nisi id non ad reprehenderit aliqua. Reprehenderit duis in labore labore nisi nostrud" +
+                    "estsint reprehenderit elit dolore proident. Tempor qui id do eu irure nulla nostrud eiusmod."}
+                tech_stack="Next.js &nbsp; React &nbsp; SocketIO &nbsp; Express"
+                hosted_link="https://kushalupreti.github.io/ReactChat"
+            />
+
+            <Project
+                image_src="/assets/react-chat.png"
+                title="React-Chat"
+                project_link="https://github.com/KushalUpreti/React-Chat"
+                project_desc={"Qui aliquip voluptate nisi id non ad reprehenderit aliqua. Reprehenderit duis in labore labore nisi nostrud" +
+                    "estsint reprehenderit elit dolore proident. Tempor qui id do eu irure nulla nostrud eiusmod."}
+                tech_stack="Next.js &nbsp; React &nbsp; SocketIO &nbsp; Express"
+                hosted_link="https://kushalupreti.github.io/ReactChat"
+            />
+
         </ul>
     </section>
 }
