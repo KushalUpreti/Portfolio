ef414c7c005287bf4954d0b011ce99d58512f7f5
Kushal Upreti <ehunt9030@gmail.com>
2022-05-03 13:46:24 +0545

    deleted api

--- pages/api/index.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-export default async function handler(req, res) {
-    const client = await MongoClient.connect(`mongodb+srv://IcyHotShoto:ClqlGSKkIunBA4ha@cluster0.dbftm.mongodb.net/form?retryWrites=true&w=majority`);
-    const db = client.db();
-    const coll = db.collection('homeform');
-    let result = await coll.find().toArray();
-    res.status(200).json(result)
-}
